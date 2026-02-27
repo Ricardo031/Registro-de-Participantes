617e8126c4742f10a3879b6de5ee8e0c718e7520
spideychill777 <spideycuentas2711@gmail.com>
2026-02-27 08:01:47 -0500

    Age and acknowledge validation created

--- main.py
+++ main.py
@@ -13,10 +13,19 @@ while n < people_registered:
     age = int(input("Please enter your age: "))
 
     acknowledge = int(input("Do you have basic acknowledge developing?\n Yes (1) \n No (2) \n : "))
+    print("")
 
     n+=1 
 
+    if age > 15 and acknowledge == 1:  
+        print("---Thanks, welcome to the proyect!---")
+
+    else: 
+    
+        print("---Sorry, you can not participate---")
+
 print("\nRegistration complete! All users have been stored.")
+
 
 
 
